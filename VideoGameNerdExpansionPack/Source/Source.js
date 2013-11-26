5bc79840e94611f49984a98fb26fc8b058ee2c87
apljee <apljee23@hotmail.com>
2013-11-25 20:29:57 -0500

    Update Source.js

--- VideoGameNerdExpansionPack/Source/Source.js
+++ VideoGameNerdExpansionPack/Source/Source.js
@@ -4,4 +4,12 @@ var NerdExpPack = {};
         NERDExpPack.addTopic = function () {
                 GDT.addTopics([
                 {
-                    id:
+                    id: "Tower Defence"
+                    name: "Tower Defence".localize("game topic"),
+                    genreWeightings: [.8, .6, .6, .8, 1, .9],
+                    audienceWeightings: [0.8, 1, 0.6]
+                    
+                }
+                });
+        };
+        
