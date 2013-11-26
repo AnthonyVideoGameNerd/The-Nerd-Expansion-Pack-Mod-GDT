e134b00c915ea1eb5b89dfab1f80fb2beb391b86
apljee <apljee23@hotmail.com>
2013-11-25 20:44:48 -0500

    Update Source.js
    Added Hitman topic, and Farming topic.

--- VideoGameNerdExpansionPack/Source/Source.js
+++ VideoGameNerdExpansionPack/Source/Source.js
@@ -6,10 +6,16 @@ var NerdExpPack = {};
                 {
                     id: "Tower Defence"
                     name: "Tower Defence".localize("game topic"),
-                    genreWeightings: [.8, .6, .6, .8, 1, .9],
+                    genreWeightings: [0.8, 0.6, 0.6, 0.8, 1, 0.9],
                     audienceWeightings: [0.8, 1, 0.6]
                     
-                }
-                });
-        };
-        
+                }, {
+                    id: "Hitman",
+                    name: "Hitman".localize("game topic"),
+                    genreWeightings: [1, 0.8, 0.7, 1, 0.6, 0.7],
+                    audienceWeightings: [0.6, 0.7, 1]
+                }, {
+                    id: "Farming",
+                    name: "Farming".localize("game topic"),
+                    genreWeightings: [0.6, 0.6, 0.7, 1, 0.9, 0.1],
+                    audienceWeightings: [1, 1, 0.8]
