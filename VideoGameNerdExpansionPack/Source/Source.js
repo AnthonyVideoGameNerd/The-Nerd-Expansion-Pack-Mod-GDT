596d38ebbf1d28c9865b331402419e499e9eb803
Anthony Jackson <anthony24778@yahoo.com>
2013-11-25 20:16:18 -0600

    added more topics- now 10 topics

--- VideoGameNerdExpansionPack/Source/Source.js
+++ VideoGameNerdExpansionPack/Source/Source.js
@@ -4,18 +4,53 @@ var NerdExpPack = {};
         NERDExpPack.addTopic = function () {
                 GDT.addTopics([
                 {
-                    id: "Tower Defence"
+                    id: "Tower Defence",
                     name: "Tower Defence".localize("game topic"),
                     genreWeightings: [0.8, 0.6, 0.6, 0.8, 1, 0.9],
                     audienceWeightings: [0.8, 1, 0.6]
-                    
                 }, {
+                    id: "Hit And Run",
+                    name: "Hit And Run".localize("game topic"),
+                    genreWeightings: [1, 0.8, 0.6, 0.7, 0.6, 0.6],
+                    audienceWeightings: [0.6, 0.7, 1]
+                }, {    
                     id: "Hitman",
                     name: "Hitman".localize("game topic"),
                     genreWeightings: [1, 0.8, 0.7, 1, 0.6, 0.7],
                     audienceWeightings: [0.6, 0.7, 1]
                 }, {
+                    id: "Theft",
+                    name: "Theft".localize("game topic"),
+                    genreWeightings: [1, 0.8, 0.6, 0.8, 0.8, 0.7],
+                    audienceWeightings: [0.6, 0.7, 1]
+                }, {    
                     id: "Farming",
                     name: "Farming".localize("game topic"),
                     genreWeightings: [0.6, 0.6, 0.7, 1, 0.9, 0.1],
                     audienceWeightings: [1, 1, 0.8]
+                }, {
+                    id: "Turn-Based Strategy",
+                    name: "Turn-Based Strategy".localize("game topic"),
+                    genreWeightings: [0.6, 0.6, 0.6, 1, 1, 0.8],
+                    audienceWeightings: [1, 1, 0.8]
+                }, {
+                    id: "Real-Time Strategy",
+                    name: "Real-Time Strategy".localize("game topic"),
+                    genreWeightings: [0.6, 0.6, 0.6, 1, 1, 0.8],
+                    audienceWeightings: [1, 1, 0.8]
+                }, {       
+                     id: "Management",
+                    name: "Management".localize("game topic"),
+                    genreWeightings: [0.6, 0.6, 0.6, 1, 1, 0.9],
+                    audienceWeightings: [1, 1, 0.9]
+                }, {      
+                     id: "Cooking",
+                    name: "Cooking".localize("game topic"),
+                    genreWeightings: [0.6, 0.6, 0.8, 1, 1, 0.8],
+                    audienceWeightings: [1, 1, 0.8]
+                }, { 
+                    id: "Airplane",
+                    name: "Airplane".localize("game topic"),
+                    genreWeightings: [0.9, 1, 0.7, 1, 1, 0.8],
+                    audienceWeightings: [1, 1, 0.9]
+                }, {       
