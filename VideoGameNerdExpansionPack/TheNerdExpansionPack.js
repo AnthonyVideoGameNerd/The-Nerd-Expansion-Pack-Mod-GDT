062e158743a0ab5608aa30429d746e7097c3e04f
Anthony Jackson <anthony24778@yahoo.com>
2014-01-04 19:42:49 -0600

    Update TheNerdExpansionPack.js

--- VideoGameNerdExpansionPack/TheNerdExpansionPack.js
+++ VideoGameNerdExpansionPack/TheNerdExpansionPack.js
@@ -21,13 +21,7 @@
 	var error = function () {
 	};
 
-	GDT.loadJs(['mods/gdt-modAPI/helpers/checks.js',
-	'mods/gdt-modAPI/api/persistence.js',
-	'mods/gdt-modAPI/api/events.js',
-	'mods/gdt-modAPI/api/platforms.js',
-	'mods/gdt-modAPI/api/topics.js',
-	'mods/gdt-modAPI/api/research.js',
-	'mods/gdt-modAPI/examples/examples.js',
-	'mods/ExpansionPack/source/source.js'
-	], ready, error);
+	GDT.loadJs([
+	'mods/VideoGameNerdExpansionPack/source/source.js',
+		], ready, error);
 })();
